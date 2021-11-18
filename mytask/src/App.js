d03d2b2c30e38fd68e4745301b488c975dada4e3
Salma Osama <76213984+salmaaosama@users.noreply.github.com>
2021-11-18 10:50:23 -0800

    files

---
+++ mytask/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Salma Osama Hamid</h1>
+    </div>
+  );
+}
+
+export default App;
